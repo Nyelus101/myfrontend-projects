084c03f8727b2522e480fc24bb39b5bba2c53043
Muonyelu Chinedu <cmuonyelu@gmail.com>
2023-08-14 13:59:51 +0100

    practice

---
+++ ASYNC_JAVASCRIPT/practice.js
@@ -0,0 +1,21 @@
+/*let sum = 0;
+const numbers = [65, 44, 12, 4];
+
+numbers.forEach((number)=>{
+	sum += number;
+});
+
+document.getElementById("demo").innerHTML = sum;
+*/
+
+
+const getData = async () => {
+    let y = await "Hello World";
+    console.log(y);
+};
+
+
+console.log(1);
+getData();
+console.log(2);
+
---
+++ Algorithmn/index.js
@@ -0,0 +1,27 @@
+/* Printing even numbers....
+function printEvenNum(){
+    for (let i = 0; i <= 10; i++) {
+        if (i % 2 === 0) {
+            console.log(i);
+        }
+    };
+};
+printEvenNum();
+*/
+
+/* Create a multiplication table with input from user....
+const number = parseInt(prompt('Enter an integer: '));
+for(let a = 1; a <= 10; a++) {
+    const result = a * number;
+    console.log(`${number} * ${a} = ${result}`);
+}
+*/
+
+/*Length Converter Function...
+const convertEl = parseInt(prompt('Enter unit in kilometers....'));
+function conV(){
+    result = convertEl * 0.621371;
+    console.log(result);
+}
+conV();
+*/
